ed2a49b40856381374361d3d7fa3b3a9d8450516
milosz.hoc <miloszhoc@gmail.com>
2020-04-07 22:02:40 +0200

    added migrations

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_file = db.Column(db.String, nullable=False, default='default')
     translated_file = db.Column(db.String, nullable=False, default='default translated')
+    translated_file_url = db.Column(db.String, nullable=False, default='default url')
 
     def __init__(self, original_file, translated_file):
         self.original_file = original_file
